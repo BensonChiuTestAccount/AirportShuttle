bf64d9b94c92e099d09fbe53d2f2805ea2f3e52e
BensonChiuTestAccount <abc1234@hotmail.com>
2012-06-24 17:52:22 -0700

    abc

--- HelloWorld_GitHub/HelloWorld_GitHub/Program.cs
+++ HelloWorld_GitHub/HelloWorld_GitHub/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace HelloWorld_GitHub
+﻿namespace HelloWorld_GitHub
 {
     class Program
     {
         static void Main(string[] args)
         {
-// hi 1 2
+            // hi 1 2 Change Made by Benson
         }
     }
 }
